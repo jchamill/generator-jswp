43aa0f07c5ff0e725d5c00f04088498dee2cdbab
JC Hamill <jchamill@creativeshampoo.com>
2018-05-29 11:15:56 -0400

    remove primary container id

--- generators/app/templates/template-parts/content-single.php
+++ generators/app/templates/template-parts/content-single.php
@@ -7,21 +7,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div id="primary-container" class="container">
-    <div class="entry-content">
-      <?php
-      the_content();
+  <div class="entry-content">
+    <?php
+    the_content();
 
-      wp_link_pages( array(
-        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', '<%= themeKey %>' ) . '</span>',
-        'after'       => '</div>',
-        'link_before' => '<span>',
-        'link_after'  => '</span>',
-        'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', '<%= themeKey %>' ) . ' </span>%',
-        'separator'   => '<span class="screen-reader-text">, </span>',
-      ) );
-      ?>
-    </div><!-- .entry-content -->
-  </div>
-
+    wp_link_pages( array(
+      'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', '<%= themeKey %>' ) . '</span>',
+      'after'       => '</div>',
+      'link_before' => '<span>',
+      'link_after'  => '</span>',
+      'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', '<%= themeKey %>' ) . ' </span>%',
+      'separator'   => '<span class="screen-reader-text">, </span>',
+    ) );
+    ?>
+  </div><!-- .entry-content -->
 </article><!-- #post-## -->
--- generators/app/templates/template-parts/content.php
+++ generators/app/templates/template-parts/content.php
@@ -7,26 +7,20 @@
 ?>
 <?php $post_class = !is_single() ? 'post-teaser' : ''; ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class($post_class); ?>>
+  <?php if (!is_single()): ?>
+    <?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h4>' ); ?>
+    <?php <%= themeKey %>_excerpt(); ?>
+  <?php else: ?>
+    <div class="post-meta">
+      <?php <%= themeKey %>_entry_meta(); ?>
+    </div>
 
-  <div id="primary-container">
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div><!-- .entry-content -->
 
-    <?php if (!is_single()): ?>
-      <?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h4>' ); ?>
-      <?php <%= themeKey %>_excerpt(); ?>
-    <?php else: ?>
-      <div class="post-meta">
-        <?php <%= themeKey %>_entry_meta(); ?>
-      </div>
-
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div><!-- .entry-content -->
-
-      <div class="post-categories">
-        <?php <%= themeKey %>_entry_taxonomies(); ?>
-      </div>
-    <?php endif; ?>
-
-  </div>
-
+    <div class="post-categories">
+      <?php <%= themeKey %>_entry_taxonomies(); ?>
+    </div>
+  <?php endif; ?>
 </article><!-- #post-## -->
