c1fd5833334860e5e6cb657bd5f5dd93723b2af9
JC Hamill <jchamill@creativeshampoo.com>
2018-06-04 16:01:06 -0400

    disable attachment pages with redirect

--- generators/app/templates/image.php
+++ generators/app/templates/image.php
@@ -5,100 +5,11 @@
  * @package <%= themeKey %>
  */
 
-get_header(); ?>
-
-<div id="primary" class="content-area">
-  <main id="main" class="site-main" role="main">
-
-    <?php while ( have_posts() ) : the_post(); ?>
-
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-        <nav id="image-navigation" class="navigation image-navigation">
-          <div class="nav-links">
-            <div class="nav-previous"><?php previous_image_link( false, 'Previous Image' ); ?></div>
-            <div class="nav-next"><?php next_image_link( false, 'Next Image' ); ?></div>
-          </div><!-- .nav-links -->
-        </nav><!-- .image-navigation -->
-
-        <header class="entry-header">
-          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        </header><!-- .entry-header -->
-
-        <div class="entry-content">
-
-          <div class="entry-attachment">
-            <?php
-            /**
-             * Filter the default image attachment size.
-             *
-             * @param string $image_size Image size. Default 'large'.
-             */
-            $image_size = apply_filters( '<%= themeKey %>_attachment_size', 'large' );
-
-            echo wp_get_attachment_image( get_the_ID(), $image_size );
-            ?>
-
-            <?php <%= themeKey %>_excerpt( 'entry-caption' ); ?>
-
-          </div><!-- .entry-attachment -->
-
-          <?php
-          the_content();
-          wp_link_pages( array(
-            'before'      => '<div class="page-links"><span class="page-links-title">Pages:</span>',
-            'after'       => '</div>',
-            'link_before' => '<span>',
-            'link_after'  => '</span>',
-            'pagelink'    => '<span class="screen-reader-text">Page </span>%',
-            'separator'   => '<span class="screen-reader-text">, </span>',
-          ) );
-          ?>
-        </div><!-- .entry-content -->
-
-        <footer class="entry-footer">
-          <?php <%= themeKey %>_entry_meta(); ?>
-          <?php
-          // Retrieve attachment metadata.
-          $metadata = wp_get_attachment_metadata();
-          if ( $metadata ) {
-            printf( '<span class="full-size-link"><span class="screen-reader-text">%1$s </span><a href="%2$s">%3$s &times; %4$s</a></span>',
-              esc_html_x( 'Full size', 'Used before full size attachment link.', '<%= themeKey %>' ),
-              esc_url( wp_get_attachment_url() ),
-              absint( $metadata['width'] ),
-              absint( $metadata['height'] )
-            );
-          }
-          ?>
-          <?php
-          edit_post_link(
-            sprintf(
-            /* translators: %s: Name of current post */
-              __( 'Edit<span class="screen-reader-text"> "%s"</span>', '<%= themeKey %>' ),
-              get_the_title()
-            ),
-            '<span class="edit-link">',
-            '</span>'
-          );
-          ?>
-        </footer><!-- .entry-footer -->
-      </article><!-- #post-## -->
-
-      <?php
-      // If comments are open or we have at least one comment, load up the comment template.
-      if ( comments_open() || get_comments_number() ) {
-        comments_template();
-      }
-
-      // Parent post navigation.
-      the_post_navigation( array(
-        'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', '<%= themeKey %>' ),
-      ) );
-      // End the loop.
-    endwhile;
-    ?>
-
-  </main><!-- .site-main -->
-</div><!-- .content-area -->
-
-<?php get_footer(); ?>
+global $post;
+if ( $post && $post->post_parent ) {
+  wp_redirect( esc_url( get_permalink( $post->post_parent ) ), 301 );
+  exit;
+} else {
+  wp_redirect( esc_url( home_url( '/' ) ), 301 );
+  exit;
+}
