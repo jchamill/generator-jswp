7d232e0b64420141ce8111534250a924e0625f8f
JC Hamill <jchamill@creativeshampoo.com>
2018-09-28 11:29:00 -0400

    hide youtube related videos and autoplay

--- generators/app/templates/js/theme/initialize.js
+++ generators/app/templates/js/theme/initialize.js
@@ -14,7 +14,16 @@
     });
 
     $('.modal-iframe').magnificPopup({
-      type: 'iframe'
+      type: 'iframe',
+      iframe: {
+        patterns: {
+          youtube: {
+            index: 'youtube.com',
+            id: 'v=',
+            src: '//www.youtube.com/embed/%id%?rel=0&autoplay=1'
+          }
+        }
+      }
     });
   });
 })(jQuery);
